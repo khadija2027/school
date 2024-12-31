97457842607eb912bec08bea825d0647bd4685be
Khadija Elmrabet <khadijaelmrabt2025@gmail.com>
2024-12-31 14:30:46 +0100

    Premier commit - ajout des fichiers existants

--- school/settings.py
+++ school/settings.py
@@ -72,14 +72,12 @@ WSGI_APPLICATION = 'school.wsgi.application'
 
 
 DATABASES = {
-    #'default': {
-      #  'ENGINE': 'django.db.backends.sqlite3',
-      #  'NAME': BASE_DIR / 'db.sqlite3',
-    #}}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }}
  
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
- 
+   
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
